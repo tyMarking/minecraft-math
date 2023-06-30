9b6787cfe44c4dbcef508099658011014397f8a2
Ty-Marking <109238565+Ty-Marking@users.noreply.github.com>
2023-06-30 16:47:15 -0400

    Kelp framework

---
+++ kelp.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+""" GENERAL MINECRAFT CONSTANTS """
+RANDOM_TICK_RATE = 3 # 3/tick
+SUBCHUNK_SIZE = 4096
+TICK_RATE = 20 # 20/s
+
+""" KELP CONSTANTS """
+KELP_GROWTH_CHANCE = 0.14 # 0.14/random tick
+
+
+def get_kelp_production(harvest_rate, harvest_height):
+    pass
+
+def plot_harvest_rate_per_height():
+    pass
+
+if __name__ == "__main__":
+    plot_harvest_rate_per_height()
